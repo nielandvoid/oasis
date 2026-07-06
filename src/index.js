7ca702a6105933df83f4d8603ead56dd2320a363
niel <nielandvoid@gmail.com>
2026-07-06 22:10:33 +0530

    set status to idle

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const client = new Client({
     GatewayIntentBits.GuildMessages,
   ],
   presence: {
-    status: 'online',
+    status: 'idle',
     activities: [{
       type: ActivityType.Custom,
       name: 'custom',
