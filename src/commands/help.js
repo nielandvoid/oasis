277d6bf6e48425c71c9d6570f6d00602a19b919e
niel <nielandvoid@gmail.com>
2026-07-06 21:15:26 +0530

    custom emojis in /help

--- src/commands/help.js
+++ src/commands/help.js
@@ -11,27 +11,27 @@ export async function execute(interaction) {
     .setDescription('Here is a list of all available commands and how to use them:')
     .addFields(
       { 
-        name: '📁 `/submit file`', 
+        name: '<:files:1523716237748732005> `/submit file`', 
         value: 'Submit file attachments (up to 5 files at once) for moderator review. You will be prompted to add a description after uploading.', 
         inline: false 
       },
       { 
-        name: '🔗 `/submit link`', 
+        name: '<:link:1523716241662021743> `/submit link`', 
         value: 'Submit a URL or web link for moderator review. You will be prompted to add a description after entering the URL.', 
         inline: false 
       },
       { 
-        name: '⚙️ `/configure`', 
+        name: '<:configure:1523716239690698762> `/configure`', 
         value: 'Set up the review channel and public resources channel for this server. (Requires *Manage Channels* permission).', 
         inline: false 
       },
       { 
-        name: '🏓 `/ping`', 
+        name: '<:pong:1523716243868221631> `/ping`', 
         value: 'Check the bot\'s websocket heartbeat and response latency.', 
         inline: false 
       },
       { 
-        name: '❓ `/help`', 
+        name: '<:questionmark:1523716245952794848> `/help`', 
         value: 'Shows this help message.', 
         inline: false 
       }
