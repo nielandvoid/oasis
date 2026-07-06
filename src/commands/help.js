f3ba133b38e1eadcdba79ab33fa505b041b41525
niel <nielandvoid@gmail.com>
2026-07-06 21:25:21 +0530

    edited /help

--- src/commands/help.js
+++ src/commands/help.js
@@ -6,33 +6,33 @@ export const data = new SlashCommandBuilder()
 
 export async function execute(interaction) {
   const helpEmbed = new EmbedBuilder()
-     .setTitle('Oasis Command Help')
-     .setColor('#ffffff')
-     .setDescription('Here is a list of all available commands and how to use them:')
+    .setTitle('Oasis 101')
+    .setColor('#ffffff')
+    .setDescription('here is a list of all available commands and how to use them:')
     .addFields(
       { 
         name: '<:files:1523716237748732005> `/submit file`', 
-        value: 'Submit file attachments (up to 5 files at once) for moderator review. You will be prompted to add a description after uploading.', 
+        value: 'submit file attachments (up to 5 files at once) for moderator review. you will be prompted to add a description after uploading.', 
         inline: false 
       },
       { 
         name: '<:link:1523716241662021743> `/submit link`', 
-        value: 'Submit a URL or web link for moderator review. You will be prompted to add a description after entering the URL.', 
+        value: 'submit a URL or web link for moderator review. you will be prompted to add a description after entering the URL.', 
         inline: false 
       },
       { 
         name: '<:configure:1523716239690698762> `/configure`', 
-        value: 'Set up the review channel and public resources channel for this server. (Requires *Manage Channels* permission).', 
+        value: 'set up the review channel and public resources channel for this server. (requires **Manage Channels**).', 
         inline: false 
       },
       { 
         name: '<:pong:1523716243868221631> `/ping`', 
-        value: 'Check the bot\'s websocket heartbeat and response latency.', 
+        value: 'check the bot\'s websocket heartbeat and response latency.', 
         inline: false 
       },
       { 
         name: '<:questionmark:1523716245952794848> `/help`', 
-        value: 'Shows this help message.', 
+        value: 'shows this help embed.', 
         inline: false 
       }
     )
